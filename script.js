f9f90da40a465d2495d6b4c2245d0d972fcdc430
Per Haagensen <pdhaagensen@gmail.com>
2020-08-17 17:17:20 +0200

    hejho

--- script.js
+++ script.js
@@ -17,11 +17,22 @@ function drawSquare(start, stop, squareX, squareY){
     ctx.stroke();
 }
 
-function drawRandomFigure(start, stop, x, y, z){
+function drawRandomFigure(start, stop, x1, y1, x2, y2){
     ctx.beginPath();
     ctx.moveTo(start, stop);
+    ctx.lineTo(start + x1, stop + y1);
+    ctx.lineTo(x1, stop + y1);
+    ctx.lineTo(x1, y2);
+    ctx.lineTo(x2, y2);
+    ctx.lineTo(start, stop);
+    ctx.fillStyle = "blue";
+    ctx.fill()
+    ctx.stroke();
 }
 
 drawSquare(10,10,100,100);
 drawSquare(100,100,12,100);
 drawSquare(400,400,100,100);
+
+drawRandomFigure(100, 400, 50, 50, 75, 50);
+drawRandomFigure(300, 100, 522, 50, 25, 30);
