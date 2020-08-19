8aa35dd5267bbae3e73733874bd79271938eb9b7
Per Haagensen <pdhaagensen@gmail.com>
2020-08-19 17:44:50 +0200

    masterpiece working  Changes to be committed: 	modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let canvas = document.getElementById("art1");
 let ctx = canvas.getContext("2d");
 
-function drawSquare(context, start, stop, squareX, squareY) {
+function drawSquare(context, color, start, stop, squareX, squareY) {
     context.beginPath();
     context.moveTo(start, stop);
     context.lineTo(start + squareX, stop);
     context.lineTo(start + squareX, stop + squareY);
     context.lineTo(start, stop + squareY);
     context.lineTo(start, stop);
-    context.fillStyle = "red";
+    context.fillStyle = color;
     context.fill();
     context.stroke();
 }
@@ -26,8 +26,9 @@ function drawRandomFigure(context, start, stop, x1, y1, x2, y2) {
     context.stroke();
 }
 
-function drawCircle(context, centerX, centerY, radius) {
+function drawCircle(context, color, centerX, centerY, radius) {
     context.beginPath();
+    context.fillStyle = color;
     context.arc(centerX, centerY, radius, 0, Math.PI * 2);
     context.closePath();
     context.fill()
@@ -41,15 +42,34 @@ function drawCircle2(context, centerX, centerY, radius) {
     context.fill();
 }
 
-drawCircle(ctx, 250, 250, 100);
-drawCircle2(ctx, 450, 200, 30);
+function drawRectangle(context, color, start, stop, x, y) {
+    context.beginPath();
+    context.moveTo(start, stop);
+    context.lineTo(start + x, stop);
+    context.lineTo(start + x, stop + y);
+    context.lineTo(start, stop + y);
+    context.closePath();
+    context.fillStyle = color;
+    context.fill();
+    context.stroke();
+}
 
-drawSquare(ctx, 10, 10, 100, 100);
-drawSquare(ctx, 100, 100, 12, 100);
-drawSquare(ctx, 400, 400, 100, 100);
+function drawTriangle(context, color, startX, startY, x, y) {
+    context.beginPath();
+    context.moveTo(startX, startY);
+    context.lineTo(startX + x, startY + y);
+    context.lineTo(startX - x, startY + y);
+    context.closePath();
+    context.fillStyle = color;
+    context.fill();
+    context.stroke();
+}
 
-drawRandomFigure(ctx, 100, 400, 50, 50, 75, 50);
-drawRandomFigure(ctx, 300, 100, 522, 50, 25, 30);
+
+drawRectangle(ctx, "#EE4C7C", 116, 58, 116, 232);
+drawTriangle(ctx, "#45A29E", 175, 290, 58, 55);
+drawSquare(ctx, "#D79922", 200, 35, 75, 75);
+drawCircle(ctx, "#C5CBE3", 237, 200, 40);
 
 
 
