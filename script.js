a39659cb3e6605c8371c0155840c36e7d9a10098
Per Haagensen <pdhaagensen@gmail.com>
2020-08-19 17:01:24 +0200

    Beautify  Changes to be committed: 	modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let canvas = document.getElementById("art1");
 let ctx = canvas.getContext("2d");
 
-function drawSquare(context, start, stop, squareX, squareY){
+function drawSquare(context, start, stop, squareX, squareY) {
     context.beginPath();
-    context.moveTo(start,stop);
+    context.moveTo(start, stop);
     context.lineTo(start + squareX, stop);
     context.lineTo(start + squareX, stop + squareY);
     context.lineTo(start, stop + squareY);
@@ -13,7 +13,7 @@ function drawSquare(context, start, stop, squareX, squareY){
     context.stroke();
 }
 
-function drawRandomFigure(context, start, stop, x1, y1, x2, y2){
+function drawRandomFigure(context, start, stop, x1, y1, x2, y2) {
     context.beginPath();
     context.moveTo(start, stop);
     context.lineTo(start + x1, stop + y1);
@@ -26,17 +26,17 @@ function drawRandomFigure(context, start, stop, x1, y1, x2, y2){
     context.stroke();
 }
 
-function drawCircle(context, centerX, centerY, radius){
+function drawCircle(context, centerX, centerY, radius) {
     context.beginPath();
-    context.arc(centerX, centerY, radius, 0, Math.PI*2);
+    context.arc(centerX, centerY, radius, 0, Math.PI * 2);
     context.closePath();
     context.fill()
 }
 
-function drawCircle2(context, centerX, centerY, radius){
+function drawCircle2(context, centerX, centerY, radius) {
     context.beginPath();
     context.fillStyle = "green";
-    context.arc(centerX, centerY, radius, 0, 3, Math.PI*2);
+    context.arc(centerX, centerY, radius, 0, 3, Math.PI * 2);
     context.closePath();
     context.fill();
 }
@@ -44,9 +44,9 @@ function drawCircle2(context, centerX, centerY, radius){
 drawCircle(ctx, 250, 250, 100);
 drawCircle2(ctx, 450, 200, 30);
 
-drawSquare(ctx, 10,10,100,100);
-drawSquare(ctx, 100,100,12,100);
-drawSquare(ctx, 400,400,100,100);
+drawSquare(ctx, 10, 10, 100, 100);
+drawSquare(ctx, 100, 100, 12, 100);
+drawSquare(ctx, 400, 400, 100, 100);
 
 drawRandomFigure(ctx, 100, 400, 50, 50, 75, 50);
 drawRandomFigure(ctx, 300, 100, 522, 50, 25, 30);
@@ -57,17 +57,9 @@ let canvas1 = document.getElementById("art2");
 let ctx2 = canvas1.getContext("2d");
 
 
-drawSquare(ctx2,10,10,100,100);
-drawSquare(ctx2,100,100,12,100);
-drawSquare(ctx2,400,400,100,100);
+drawSquare(ctx2, 10, 10, 100, 100);
+drawSquare(ctx2, 100, 100, 12, 100);
+drawSquare(ctx2, 400, 400, 100, 100);
 
-drawRandomFigure(ctx2,100, 400, 50, 50, 75, 50);
-drawRandomFigure(ctx2,300, 100, 522, 50, 25, 30);
-
-
-
-
-
-
-
-
+drawRandomFigure(ctx2, 100, 400, 50, 50, 75, 50);
+drawRandomFigure(ctx2, 300, 100, 522, 50, 25, 30);
