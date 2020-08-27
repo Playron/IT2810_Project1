df51df7732e8e3b81a507038fd4a2848c0cf4ed9
Per Haagensen <pdhaagensen@gmail.com>
2020-08-27 19:16:34 +0200

    redid the interactivity on he canvas element now. Now it is a little bit more fun  Changes to be committed: 	modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -47,6 +47,7 @@ function drawCircle2(context, color, centerX, centerY, radius) {
 }
 /*Instead of using rect function i built my own. Lets me specify color and makes interactivity easier */
 function drawRectangle(context, color, start, stop, x, y) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
     context.beginPath();
     context.moveTo(start, stop);
     context.lineTo(start + x, stop);
@@ -101,10 +102,20 @@ drawCircle(ctx, "#41B3A3", 100, 200, 50);
 let startX = 116;
 let startY = 35;
 let circleRadius = 35;
+let circleX = 237;
+let circleY = 200;
+let circle2X = 100;
+let circle2Y = 200;
 let rectStartX = 116;
 let rectStartY = 58;
 let rectX = 116;
 let rectY = 232;
+let triangleX = 175;
+let triangleY = 290;
+let squareX = 200;
+let squareY = 35;
+let triangle2X = 116;
+let triangle2Y = 35;
 
 /*EventListener that will change the canvas image by clicking on it*/
 $(document).ready(function () {
@@ -115,10 +126,12 @@ $(document).ready(function () {
         drawSquare(ctx, randomColorSelector(), 200, 35, 75, 75);
         drawCircle(ctx, randomColorSelector(), 237, 200, circleRadius);
         drawCircle(ctx, randomColorSelector(), 100, 200, 50);
+        draw(ctx);
+        i++;
         /*The code from here will change the appearance of teh canvas-figures.
         This values can be changed to change the interactivity effect.
         */
-        circleRadius += 4;
+        /*circleRadius += 4;
         rectStartX -= 2;
         rectStartY += 4;
         rectX += 2;
@@ -131,7 +144,7 @@ $(document).ready(function () {
         if (rectStartY >= 100) {
             rectStartY -= 30;
             rectStartX += 80;
-        }
+        }*/
     });
 });
 /*Listens to SVG-window and change all colors if clicked*/
@@ -158,9 +171,94 @@ $(document).ready(function () {
             show = "more"
             $("#btn1").text("Show less");
         } else {
-            $("#show-more").hide(100);
+            $("#show-more").hide(1000);
             show = "less";
             $("#btn1").text("Show more");
+        }
+    });
+});
+
+
+let i = 0;
+let id;
+
+function draw(context) {
+    id = requestAnimationFrame(draw);
+    drawRectangle(ctx, randomColorSelector(), rectStartX, rectStartY, rectX, rectY);
+    drawTriangle(ctx, randomColorSelector(), triangleX, triangleY, 58, 55);
+    drawTriangle(ctx, randomColorSelector(), triangle2X, triangle2Y, -50, 70);
+    drawSquare(ctx, randomColorSelector(), squareX, squareY, 75, 75);
+    drawCircle(ctx, randomColorSelector(), circleX, circleY, 35);
+    drawCircle(ctx, randomColorSelector(), circle2X, circle2Y, 50);
+    if (i == 1) {
+        rectStartX -= 0.1;
+    }
+    if (i == 2) {
+        rectStartX += 0.1;
+        triangleY -= 0.1;
+        rectStartX += 0.2;
+        rectY += 0.2;
+    }
+    if (i == 3) {
+        rectStartX -= 0.1;
+        triangleY += 0.1;
+        circleY -= 0.1;
+        circleX -= 0.1;
+    }
+    if (i == 4) {
+        rectStartX -= 0.1;
+        triangleY += 0.1;
+        circleY += 0.1;
+        circleX += 0.1;
+        squareX -= 0.5;
+        squareY += 0.5;
+        triangle2X += 0.5;
+        triangle2Y + -1;
+    }
+    if (i == 5) {
+        rectStartX += 0.1;
+        triangleY -= 0.1;
+        circleY -= 0.1;
+        circleX -= 0.1;
+        circle2Y -= 0.1;
+        triangle2X -= 0.5;
+        triangle2Y += 0.5;
+    }
+    if (i == 8) {
+        i = 0;
+        cancelAnimationFrame(id);
+    }
+}
+
+$(document).ready(function () {
+    $('#art1').mouseleave(function () {
+        drawRectangle(ctx, randomColorSelector(), rectStartX, rectStartY, rectX, rectY);
+        drawTriangle(ctx, randomColorSelector(), triangleX, triangleY, 58, 55);
+        drawTriangle(ctx, randomColorSelector(), 116, 35, -50, 70);
+        drawSquare(ctx, randomColorSelector(), 200, 35, 75, 75);
+        drawCircle(ctx, randomColorSelector(), circleX, circleY, 35);
+        drawCircle(ctx, randomColorSelector(), circle2X, circle2Y, 50);
+        startX = 116;
+        startY = 35;
+        circleRadius = 35;
+        circleX = 237;
+        circleY = 200;
+        circle2X = 100;
+        circle2Y = 200;
+        rectStartX = 116;
+        rectStartY = 58;
+        rectX = 116;
+        rectY = 232;
+        triangleX = 175;
+        triangleY = 290;
+        squareX = 200;
+        squareY = 35;
+        triangle2X = 116;
+        triangle2Y = 35;
+        for (let j = 0; j <= 7; j++) {
+            if (j == i) {
+                cancelAnimationFrame(id);
+            }
         }
     });
 });
